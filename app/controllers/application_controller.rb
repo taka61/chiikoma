fa64fbee16aefe7134ecc9c658a3a8f804f7b4ac
taka61 <taka.tennisloveup61@gmail.com>
2022-02-14 11:16:05 +0900

    アカウント周りのレイアウトを整えた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
     chiikomas_path
