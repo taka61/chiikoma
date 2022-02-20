342731b3a530aef98ea68038b5da9936baaf2956
taka61 <taka.tennisloveup61@gmail.com>
2022-02-20 14:34:23 +0900

    ユーザー情報編集後のリダイレクト先を一覧画面へ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(_resource)
-    chiikomas_path
-  end
-
-  def after_sign_up_path_for(_resource)
-    chiikomas_path
-  end
-
-  def after_sign_out_path_for(_resource)
-    root_path
-  end
-
   def configure_permitted_parameters
     added_attrs = %i[email name password]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  authenticated :user do
+    root "chiikomas#index", as: "user_authenticated_root"
+  end
   root 'top#index'
-
   devise_for :users
+
   resources :chiikomas do
     collection do
       get 'done'
