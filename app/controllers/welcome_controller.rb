5ee223feb7a841acf5e1d028ca57a55eec629ad7
taka61 <taka.tennisloveup61@gmail.com>
2022-02-24 12:20:29 +0900

    利用規約を制定

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class WelcomeController < ApplicationController
+  def index; end
+  def tos; end
+  def privacy_policy; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get 'tos', to: 'welcome#tos', as: 'tos'
+  get 'privacy_policy', to: 'welcome#privacy_policy', as: 'privacy_policy'
+
   authenticated :user do
     root 'chiikomas#index', as: 'user_authenticated_root'
   end
-  root 'top#index'
+
   devise_for :users
 
   resources :chiikomas do
