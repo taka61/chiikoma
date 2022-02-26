645b53d85c16c24b21040b5059e1efc2561f3669
taka61 <taka.tennisloveup61@gmail.com>
2022-02-26 14:50:08 +0900

    rubocopの指摘を修正

--- test/system/static_pages_test.rb
+++ test/system/static_pages_test.rb
@@ -3,16 +3,15 @@
 require 'application_system_test_case'
 
 class StaticPagesTest < ApplicationSystemTestCase
-  test "visit policy" do
+  test 'visit policy' do
     visit privacy_policy_path
     assert_text '本サービスは、ユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。'
     assert_text '本サービスは、必要に応じて、このプライバシーポリシーの内容を変更します。'
   end
 
-  test "visit tos" do
+  test 'visit tos' do
     visit tos_path
     assert_text '本規約に従って、本サービスをご利用いただきます。'
     assert_text '本規約の解釈にあたっては、日本法を準拠法とします。'
   end
-    
 end
