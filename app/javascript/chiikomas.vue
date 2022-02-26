88d140577e930deb7a28e113f9c2189001c7bccd
taka61 <taka.tennisloveup61@gmail.com>
2022-02-26 13:33:42 +0900

    日替わりちいこまの数を増やした

--- app/javascript/chiikomas.vue
+++ app/javascript/chiikomas.vue
@@ -26,7 +26,7 @@
                 追加する
               </button>
               <button
-                class="button js_modal_close modal_close"
+                class="button-delete js_modal_close modal_close"
                 type="button">
                 閉じる
               </button>
@@ -69,7 +69,6 @@
 
 <script>
 import axios from 'axios'
-
 export default {
   data() {
     return {
@@ -78,18 +77,22 @@ export default {
       frequency_of_experience:'',
       cost_of_solution:'',
       is_made_by_admin: '',
-      title:'',
-      titles: ['衣替えを行う','キッチンの換気扇を掃除する','保険料の見直し',
-               'パソコンの掃除']
+      title:''
     }
   },
   mounted () {
     this.getChiikoma();
-    this.random();
+    this.randomChiikoma ();
   },
   methods: {
-    random () {
-     this.title = this.titles[Math.floor(Math.random() * this.titles.length)]
+    randomChiikoma () {
+      const titles = ['衣替えを行いたい','キッチンの換気扇が汚れている','トイレの匂いが気になる','睡眠中に足がつりやすい',
+                   'パソコンのディスプレイをの汚れが気になる','洗濯機のフィルターごみが溜まっている','網戸の締まりが悪い',
+                   '睡眠中に足がつりやすい', '自転車の鍵を探すのが毎回面倒（ナンバーロック式などの鍵に替える?）',
+                   '網戸の締まりが悪い','洗面台の鏡のウロコを取りたい','エアコンのクリーニング','スマホの電池の減りが気になる',
+                   '眼精疲労','肩がずっと凝っている','自転車のタイヤの空気が抜けてきた','電球の交換', '不用品を処分したい'
+                    ]
+      this.title = titles[Math.floor(Math.random() * titles.length)]
     },
     getChiikoma () {
       axios.get('/api/chiikomas')
@@ -106,7 +109,7 @@ export default {
           level_of_problem: 5,
           frequency_of_experience: 5,
           cost_of_solution: 5,
-          is_made_by_admin: true
+          total_points:15
       }).then(response => (
         window.location.href ='/chiikomas'
       ))
