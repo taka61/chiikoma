b1e849d783d84d2396ab80792effeb5b5e0ab6bb
taka61 <taka.tennisloveup61@gmail.com>
2022-03-12 13:57:03 +0900

    コントローラーに書いていたメソッドを一部モデルへ切り出し

--- app/controllers/hassles/achievement_controller.rb
+++ app/controllers/hassles/achievement_controller.rb
@@ -5,45 +5,23 @@ module Hassles
     before_action :authenticate_user!
 
     def index
-      @hassles = current_user.hassles
-      format_date
-      fetch_registration_chiikoma
-      fetch_solution_chiikoma
+      display_one_week
+      @registration_numbers = current_user.fetch_registration_numbers
+      @registration_points = current_user.fetch_registration_points
+      @solution_numbers = current_user.fetch_solution_numbers
+      @solution_points = current_user.fetch_solution_points
       fetch_all_chiikoma
-      show_chart
+      @registration_chiikoma_and_solution_chiikoma = current_user.show_chart
     end
 
-    def format_date
+    def display_one_week
       @this_monday = Time.current.beginning_of_week
       @this_sunday = Time.current.end_of_week
-    end
-
-    def fetch_registration_chiikoma
-      @registration_numbers = @hassles.where(is_made_by_admin: false).this_week.count
-      @registration_points = @hassles.this_week.sum(:registration_points)
-    end
-
-    def fetch_solution_chiikoma
-      @solution_numbers = @hassles.solved_on_this_week.count
-      @solution_points = @hassles.solved_on_this_week.sum(:total_points)
     end
 
     def fetch_all_chiikoma
       @all_chiikoma_numbers = @registration_numbers + @solution_numbers
       @all_chiikoma_points = @registration_points + @solution_points
-    end
-
-    def show_chart
-      @registration_chiikoma_and_solution_chiikoma = [
-        {
-          name: '解決した!',
-          data: current_user.hassles.within_3_months.group_by_week(:solved_on).sum(:total_points)
-        },
-        {
-          name: '登録した!',
-          data: current_user.hassles.within_3_months.group_by_week(:created_at).sum(:registration_points)
-        }
-      ]
     end
   end
 end
