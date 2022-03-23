c56f59813532cd821c2e2c7e3ef8a0b534e03ffd
taka61 <taka.tennisloveup61@gmail.com>
2022-03-23 16:11:32 +0900

    1週間の頑張り画面内の日付も直接viewに書く形へ変更

--- app/controllers/hassles/achievement_controller.rb
+++ app/controllers/hassles/achievement_controller.rb
@@ -4,15 +4,6 @@ module Hassles
   class AchievementController < ApplicationController
     before_action :authenticate_user!
 
-    def index
-      display_one_week
-    end
-
-    private
-
-    def display_one_week
-      @this_monday = Time.current.beginning_of_week
-      @this_sunday = Time.current.end_of_week
-    end
+    def index; end
   end
 end
